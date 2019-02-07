b212ab2ac928f8126b40ebc24aa3c76659a216d2
Georgeana Dias <geanamonte@gmail.com>
2019-02-07 17:53:08 -0200

    Tratamento do JSON, formatação de data, etc

--- app/src/main/java/com/example/stormy/MainActivity.java
+++ app/src/main/java/com/example/stormy/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -15,40 +22,90 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private CurrentWeather currentWeather;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         String apiKey = "cfb83073e2041424d85283c1778f9c8e";
-        //double latitude = 37.8267;
-        double latitude = 99.999;
+        double latitude = 37.8267;
         double longitude = -122.4233;
         String forecastURL = "https://api.darksky.net/forecast/"
                 + apiKey + "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
+        if(isNetworkAvailable()){
 
-        Request request = new Request.Builder()
-                .url(forecastURL)
-                .build();
+            OkHttpClient client = new OkHttpClient();
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure (Call call, IOException e){
-            }
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
 
-            @Override
-            public void onResponse (Call call, Response response) throws IOException {
-                if (response.isSuccessful()){
-                    Log.v(TAG, response.body().string());
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
                 }
-                else{
-                    alertUserAboutError();
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String jsonData = response.body().string();
+                    Log.v("JSON", jsonData);
+                    if (response.isSuccessful()) {
+
+                        try {
+                            currentWeather = getCurrentDetails(jsonData);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    } else {
+                        alertUserAboutError();
+                    }
                 }
-            }
-        });
+            });
+        }
+    }
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
+        JSONObject forecast = new JSONObject(jsonData);
+
+        String timezone = forecast.getString("timezone");
+        Log.i("From JSON", timezone);
+
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setLocationLabel("Alcatraz Island, CA");
+        currentWeather.setPrecipityChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimezone(timezone);
+
+        Log.d(TAG, currentWeather.getFormattedTime());
+
+        return currentWeather;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        else{
+            Toast.makeText(this, getString(R.string.network_unvailable), Toast.LENGTH_LONG);
+        }
+
+        return isAvailable;
     }
 
     private void alertUserAboutError() {
