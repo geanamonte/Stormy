b20b24973fa9686727c9e1abdefa8cfaf9d54342
Georgeana Dias <geanamonte@gmail.com>
2019-02-07 15:09:24 -0200

    Comunicação com OkHTTP e API feita

--- app/src/main/java/com/example/stormy/MainActivity.java
+++ app/src/main/java/com/example/stormy/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "cfb83073e2041424d85283c1778f9c8e";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastURL = "https://api.darksky.net/forecast/"
+                + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure (Call call, IOException e){
+            }
+
+            @Override
+            public void onResponse (Call call, Response response) throws IOException {
+                if (response.isSuccessful()){
+                    Log.v(TAG, response.body().string());
+                }
+            }
+        });
     }
+
 }
